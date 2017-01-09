8bd5fbe130a2d3051948a99b976ff35fa50557c6
roudique <roudique@gmail.com>
2017-01-09 10:22:49 -0500

    Add parsing address

--- Orderify/Model/Model.swift
+++ Orderify/Model/Model.swift
@@ -13,7 +13,7 @@ protocol JSONInitializable {
     init?(json: JSON)
 }
 
-struct Address {
+struct Address : JSONInitializable {
     var firstName   : String
     var lastName    : String
     
@@ -27,6 +27,33 @@ struct Address {
     var countryCode  : String
     var provinceCode : String
     
+    init?(json: JSON) {
+        let firstName   = json["first_name"].string
+        let lastName    = json["last_name"].string
+        
+        let address1    = json["address1"].string
+        let country     = json["country"].string
+        
+        let countryCode  = json["country_code"].string
+        let provinceCode = json["province_code"].string
+        
+        if let firstName = firstName, let lastName = lastName, let address1 = address1, let country = country, let countryCode = countryCode, let provinceCode = provinceCode {
+            self.firstName  = firstName
+            self.lastName   = lastName
+            
+            self.address1   = address1
+            self.address2   = json["address2"].string
+            self.city       = json["city"].string
+            self.zip        = json["zip"].string
+            self.province   = json["province"].string
+            self.country    = country
+            
+            self.countryCode  = countryCode
+            self.provinceCode = provinceCode
+        } else {
+            return nil
+        }
+    }
 }
 
 class Item : JSONInitializable {
